8ae76c7f82ff8b001249988ee891800b6d1ac252
jhta <jhta@DESKTOP-ABVQGVO>
2024-05-28 17:06:08 +0900

    boolean

---
+++ src/var/VarDemo6.java
@@ -0,0 +1,23 @@
+package var;
+
+public class VarDemo6 {
+
+	public static void main(String[] args) {
+		
+		// 불린형 변수
+		/*
+		 * 불린형 타입의 변수는 오직 true, false 2가지 값만 사용 가능
+		 * false나 true라는 글자를 저장하는 것이 아닌 출력할 수 있게끔 해주는 어떤 미지의 수
+		 */
+		
+		boolean isPassed = false;
+		boolean isMarried = true;
+		boolean isDeleted = false;
+		boolean hasElements = true;
+		
+		System.out.println(isPassed);
+		System.out.println(isMarried);
+		System.out.println(isDeleted);
+		System.out.println(hasElements);
+	}
+}
