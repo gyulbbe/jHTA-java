2917c74cc4b7ef94cfc0bd1d3684207b9099ef2d
jhta <gyulbbe@gmail.com>
2024-05-31 10:32:47 +0900

    구구단

---
+++ src/loop/ForDemo8.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class ForDemo8 {
+
+	public static void main(String[] args) {
+
+		//구구단
+		for(int i = 1; i<= 9; i++) {
+			for(int j = 2; j <= 9; j++) {
+				System.out.print(j + "*" + i + "=" + (i * j) + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
